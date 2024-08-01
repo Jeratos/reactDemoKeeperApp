e8c1753a1bff6088e3a67b63de34931221e2f2e5
ayush <ayushchouhan417@gmail.com>
2024-08-01 22:06:08 +0530

    first commit

---
+++ src/component/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <FirstPageIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
